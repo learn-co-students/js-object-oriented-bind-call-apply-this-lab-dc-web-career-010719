5d5d8ca0fc83ffa3fc7ac1608fd22bce12b6584c
Dusty211 <kylebenz@hotmail.com>
2019-02-25 16:29:20 -0500

    all done

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 //Your code here
+
+function justInvoke(fnc) {
+  return fnc()
+}
+
+function setThisWithCall(fnc, thisValue, arg) {
+  return fnc.call(thisValue, arg)
+}
+
+function setThisWithApply(fnc, thisValue, args) {
+  return fnc.apply(thisValue, args)
+}
+
+function returnNewFunctionOf(functionToBeCopied, thisValue) {
+  return functionToBeCopied.bind(thisValue)
+}
